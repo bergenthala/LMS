1533435312fd26c577fe389639985a8ce3264fb1
bergenthala <bergenthalandrew@gmail.com>
2023-04-21 21:15:24 -0600

    Fixed Grades

--- LMS/Controllers/ProfessorController.cs
+++ LMS/Controllers/ProfessorController.cs
@@ -456,7 +456,7 @@ namespace LMS_CustomIdentity.Controllers
 
 
             uint totalWeight = 0;
-            uint totalScore = 0;
+            double totalScore = 0;
             foreach (var category in asgcat.ToArray()) {
                 uint totalPoints = 0;
                 uint maxPoints = 0;
@@ -496,7 +496,7 @@ namespace LMS_CustomIdentity.Controllers
                     continue;
                 }
 
-                uint newGrade = totalPoints / maxPoints;
+                double newGrade = (double)totalPoints / maxPoints;
 
                 // Calculate category weight
                 newGrade *= category.Weight;
@@ -513,17 +513,17 @@ namespace LMS_CustomIdentity.Controllers
 
             Dictionary<double, string> valueToGrade = new Dictionary<double, string>
             {
-                {4.0, "A"},
-                {3.7, "A-"},
-                {3.3, "B+"},
-                {3.0, "B"},
-                {2.7, "B-"},
-                {2.3, "C+"},
-                {2.0, "C"},
-                {1.7, "C-"},
-                {1.3, "D+"},
-                {1.0, "D"},
-                {0.0, "E"}
+                {93, "A"},
+                {90, "A-"},
+                {87, "B+"},
+                {83, "B"},
+                {80, "B-"},
+                {77, "C+"},
+                {73, "C"},
+                {70, "C-"},
+                {63, "D+"},
+                {60, "D"},
+                {0, "E"}
             };
 
             string finalGrade = "";
